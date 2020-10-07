41783af0afda7b1dfd4777e4c01cbbb9321bce8f
Mejo <mejo@Nestaways-MBP.domain.name>
2020-10-07 23:05:00 +0530

    initial commit

---
+++ lib/SplashScreen/SplashScreen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+// splash screen
+class SplashScreen extends StatelessWidget {
+  //  layout for logo
+  final Widget logoView = Expanded(
+      child: Center(
+    child: Image(image: AssetImage('assets/logo.png'), width: 200, height: 200),
+  ));
+
+  // layout for version number
+  final Widget versionView = Container(
+    padding: const EdgeInsets.all(10),
+    child: Text("Version 1.0.0", softWrap: true),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      color: Colors.blue,
+      child: Column(
+        children: <Widget>[logoView, versionView],
+      ),
+    ));
+  }
+}
