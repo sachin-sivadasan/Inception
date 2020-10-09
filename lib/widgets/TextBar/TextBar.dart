47a30bf48f23c3efbfb701ac8de9bbf9fd1b4315
Mejo <mejo@Nestaways-MBP.domain.name>
2020-10-09 18:04:47 +0530

    height fixed

--- lib/widgets/TextBar/TextBar.dart
+++ lib/widgets/TextBar/TextBar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class TextBar extends StatelessWidget {
   final onTap;
@@ -8,43 +9,46 @@ class TextBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(16.0),
-      child: Row(
-        children: [
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.yellow,
-                  borderRadius: BorderRadius.circular(6.0)),
-              margin:
-                  EdgeInsets.only(top: 7.0, bottom: 7.0, left: 8.0, right: 8.0),
-              child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'bacba',
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(6.0)),
-                        borderSide:
-                            BorderSide(width: 0.0, color: Colors.transparent)),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(6.0)),
-                        borderSide:
-                            BorderSide(width: 0.0, color: Colors.transparent)),
-                  ),
-                  keyboardType: TextInputType.number),
+      child: Container(
+        height: 60.0,
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(6.0)),
+                child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'bacba',
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(6.0)),
+                          borderSide: BorderSide(
+                              width: 0.0, color: Colors.transparent)),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(6.0)),
+                          borderSide: BorderSide(
+                              width: 0.0, color: Colors.transparent)),
+                    ),
+                    keyboardType: TextInputType.number),
+              ),
             ),
-          ),
-          // GestureDetector(
-          //   onTap: () => {},
-          // child:
-          // Expanded(
-          // child:
-          Container(
-            decoration: BoxDecoration(color: Colors.red),
-            // padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
-            child: Text('MB'),
-          ),
-          // )
-          // )
-        ],
+            // GestureDetector(
+            //   onTap: () => {},
+            // child:
+            // Expanded(
+            //   child:
+            Container(
+              height: double.infinity,
+              // constraints: BoxConstraints.expand(),
+              decoration: BoxDecoration(color: Colors.green),
+              // padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+              child: Text('MB'),
+            ),
+            // )
+            // )
+          ],
+        ),
       ),
     );
   }
